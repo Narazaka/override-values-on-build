edbaec59c5da95382b6c3b8e2eed197f43cea553
Narazaka <info@narazaka.net>
2025-08-04 22:24:52 +0900

    IEditorOnly

--- Runtime/OverrideValuesOnBuild.cs
+++ Runtime/OverrideValuesOnBuild.cs
@@ -1,12 +1,13 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using VRC.SDKBase;
 
 [assembly: InternalsVisibleTo("Narazaka.VRChat.OverrideValuesOnBuild.Editor")]
 
 namespace Narazaka.VRChat.OverrideValuesOnBuild
 {
     [AddComponentMenu("OverrideValuesOnBuild")]
-    public class OverrideValuesOnBuild : MonoBehaviour
+    public class OverrideValuesOnBuild : MonoBehaviour, IEditorOnly
     {
         [SerializeField] public Component target;
         [SerializeField] public OverrideValue[] overrideValues = new OverrideValue[0];
